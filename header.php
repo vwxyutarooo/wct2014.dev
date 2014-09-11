5aff4637e713186bf8aaf14dab35ebe15979a27a
featherplain <featherplain3212@gmail.com>
2014-09-11 22:04:56 +0900

    modify media queries for footer and header.php && set footer widget for sponsers

--- header.php
+++ header.php
@@ -50,7 +50,7 @@
 			</div>
 
 			<nav id="primary-navigation" class="site-navigation primary-navigation" role="navigation">
-				<button class="menu-toggle"></button>
+				<button class="menu-toggle"><?php _e( 'primary-navigation', 'twentyfourteen' ); ?></button>
 				<a class="screen-reader-text skip-link" href="#content"><?php _e( 'Skip to content', 'twentyfourteen' ); ?></a>
 				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
 			</nav>
