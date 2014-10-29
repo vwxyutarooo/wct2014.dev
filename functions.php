39c717c6c3add5d460e0e4476a7904792f69c97e
Yutaro <yutaro@studio-rubbish.com>
2014-10-29 16:51:25 +0900

    init functions.php

---
+++ functions.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * wct2014 functions and definitions
+ *
+ * Set up the content width value based on the theme's design.
+ *
+ * @see twentyfourteen_content_width()
+ *
+ * @since wct2014 1.0
+ */
+
+/**
+ * Enqueue scripts and styles for the front end.
+ *
+ * @since Twenty Fourteen 1.0
+ */
+function wct2014_scripts() {
+	wp_enqueue_style( 'wct2014', get_stylesheet_directory_uri() . '/wct2014.css', array( 'twentyfourteen-style' ) );
+}
+add_filter( 'wp_enqueue_scripts', 'wct2014_scripts' );
+
+function wct2014_body_class( $classes ) {
+	if ( is_page_template( 'page-templates/page-sessions.php' ) ) {
+		$classes[] = 'full-width';
+	}
+	return $classes;
+}
+add_filter( 'body_class', 'wct2014_body_class' );
